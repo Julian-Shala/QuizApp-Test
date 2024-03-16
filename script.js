6eeba3c58e05f499e71ff4490dd111512b31befd
Julian Shala <julianshala@mbp-von-julian.speedport.ip>
2024-03-16 22:41:25 +0100

    JSON Array added

---
+++ script.js
@@ -0,0 +1,44 @@
+let questions = [
+  {
+    question: "Wer hat HTML erfunden?",
+    answer_1: "Robbie Williams",
+    answer_2: "Lady Gaga",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+  },
+  {
+    question: "In welchem Land wurde das Fußballspiel erfunden?",
+    answer_1: "USA",
+    answer_2: "Deutschland",
+    answer_3: "Großbritannien",
+    answer_4: "Brasilien",
+    right_answer: 3,
+  },
+  {
+    question: "Wie lautet der Weltfußballverband, mit Sitz in der Schweiz?",
+    answer_1: "FIFA",
+    answer_2: "WHO",
+    answer_3: "NFL",
+    answer_4: "UEFA",
+    right_answer: 1,
+  },
+  {
+    question:
+      "In welcher Stadt fand das Finale der Fußball-Weltmeisterschaft 2006 statt?",
+    answer_1: "London",
+    answer_2: "Berlin",
+    answer_3: "Paris",
+    answer_4: "Madrid",
+    right_answer: 2,
+  },
+  {
+    question:
+      "Welcher Verein hält den Rekord für die meisten gewonnenen deutschen Fußball-Meisterschaften?",
+    answer_1: "Borussia Dortmund",
+    answer_2: "Werder Bremen",
+    answer_3: "FC Bayern München",
+    answer_4: "VfB Stuttgart",
+    right_answer: 3,
+  },
+];
